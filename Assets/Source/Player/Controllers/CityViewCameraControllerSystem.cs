c7a11ff5baa747d262fd649d7e1525a93432b538
nick.atkocaitis <n.atkocaitis@gmail.com>
2020-05-05 19:47:56 -0400

    Checking in autogenerated input system assets

---
+++ Assets/Source/Player/Controllers/CityViewCameraControllerSystem.cs
@@ -0,0 +1,15 @@
+using Unity.Entities;
+using UnityEngine.InputSystem;
+
+class CityViewCameraControllerSystem : SystemBase
+{
+    protected override void OnCreate()
+    {
+        InputSystem.OnEv
+    }
+
+    protected override void OnUpdate()
+    {
+
+    }
+}
