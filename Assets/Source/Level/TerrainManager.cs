0d396bcd2520ac6434523a37ef71b21e360c0edc
nick.atkocaitis <n.atkocaitis@gmail.com>
2020-12-22 19:18:46 -0500

    Playing around with terrain generation.  Next step is to figure out how to properly use Perlin Noise to the fullest effect, and/or  how to overlay other noise algorithms.  Additionally, need to figure out how to do some kind of 'post processing' to normalize things like jagged edges and bumpy terrain

--- Assets/Source/Level/TerrainManager.cs
+++ Assets/Source/Level/TerrainManager.cs
@@ -4,25 +4,67 @@ using Megalopolis.Level;
 
 namespace Megalopolis.Level
 {
-    public class TerrainManager : ComponentSystem
+    public class TerrainManager : MonoBehaviour
     {
-        private bool levelGenerated = false;
+        public int width = 256;
+        public int height = 256;
+        public int depth = 20;
+        public float scale = 2;
 
-        protected override void OnUpdate()
+        public Terrain terrain;
+
+        public void Start()
         {
-            if (this.levelGenerated == false)
-            {
-                this.generateTerrainHeightmap();
-            }
+            terrain.terrainData = GenerateTerrain(this.terrain.terrainData);
         }
 
-        private void generateTerrainHeightmap()
+        private TerrainData GenerateTerrain(TerrainData terrainData)
         {
-            // Ugh.  Terrain not supported in DOTS right now.  This is horrible...
-            Entities.WithAll<TestTerrainComponent>().ForEach((Entity e) =>
+            terrainData.heightmapResolution = width + 1;
+            terrainData.size = new Vector3(this.width, depth, this.height);
+
+            float[,] heightMap = this.generateHeights();
+            heightMap = this.normalizeHeightMap(heightMap);
+            terrainData.SetHeights(0, 0, heightMap);
+
+            return terrainData;
+        }
+
+        private float[,] generateHeights()
+        {
+            float[,] heights = new float[this.width, this.height];
+            for (int x = 0; x < this.width; x++)
+            {
+                for (int y = 0; y < this.height; y++)
+                {
+                    heights[x, y] = calculatePointHeight(x, y);
+                }
+            }
+
+            return heights;
+        }
+
+        private float[,] normalizeHeightMap(float[,] rawMap)
+        {
+            foreach (float xCoord in rawMap)
             {
 
-            });
+            }
+            return rawMap;
+        }
+
+        private float calculatePointHeight(int x, int y)
+        {
+            float xCoord = (float)x / this.width * this.scale;
+            float yCoord = (float)y / this.height * this.scale;
+
+            float noise = Mathf.PerlinNoise(xCoord, yCoord);
+            noise = Mathf.PerlinNoise(
+                xCoord * noise,
+                yCoord * noise
+            );
+
+            return noise;
         }
     }
 }
